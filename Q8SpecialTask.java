8d20134b9510959c7db8bf985e31b5b0298aa65a
Vinay-Parida <60653809+Vinay-Parida@users.noreply.github.com>
2020-02-24 16:11:18 +0530

    Assessment

--- Q8SpecialTask.java
+++ Q8SpecialTask.java
@@ -4,12 +4,15 @@ class SpecailTask {
     Integer stack[] = new Integer[100];
     int MAX_SIZE = stack.length - 1;
     int top = -1;
+    int min = 99999;
 
     void push(Integer value) {
         if (top == MAX_SIZE) {
             System.out.println("Stack is full");
         } else {
             top += 1;
+            if(value< min)
+                min = value;
             stack[top] = value;
             System.out.println("Value: " + value + " added at " + top);
         }
@@ -20,17 +23,19 @@ class SpecailTask {
             System.out.println("Stack is already empty");
         } else {
             System.out.println(stack[top] + " poped");
+            if(stack[top] == min){
+                for (int i = 0; i < top; i++) {
+                    if (stack[i] < min)
+                        min = stack[i];
+                }
+            }
+
             stack[top] = null;
             top -= 1;
         }
     }
 
     void getMin() {
-        int min = stack[top];
-        for (int i = 0; i < top; i++) {
-            if (stack[i] < min)
-                min = stack[i];
-        }
         System.out.println(min + " is minimum");
     }
 
