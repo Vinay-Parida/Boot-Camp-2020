b53164835e92be23c09e8ae5157f4ec64e1a4eda
Vinay <viny.parida@tothenew.com>
2020-03-19 02:33:22 +0530

    JPA#

---
+++ AuthoredBook/src/test/java/com/example/AuthoredBook/AuthoredBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AuthoredBook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthoredBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
